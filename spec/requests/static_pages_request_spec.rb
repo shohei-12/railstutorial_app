23f55086095d5e9eecba79c150cdf5ca25917332
shohei-12 <shoheimaeda10@gmail.com>
2020-05-20 00:24:06 +0900

    Add test to check the title

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -2,24 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
 
+  before do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET /home" do
-    it "returns http success" do
-      get "/static_pages/home"
+    it "show home page" do
+      get static_pages_home_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "Home | #{@base_title}"
     end
   end
 
   describe "GET /help" do
-    it "returns http success" do
-      get "/static_pages/help"
+    it "show help page" do
+      get static_pages_help_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "Help | #{@base_title}"
     end
   end
 
   describe "GET /about" do
-    it "returns http success" do
-      get "/static_pages/about"
+    it "show about page" do
+      get static_pages_about_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "About | #{@base_title}"
     end
   end
 
